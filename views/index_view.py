258d8c19b60fa3a213c5cf3634b0d9ae2dda0829
PARK KYOUNGJUN <kjpark@uclab.re.kr>
2015-09-17 20:07:11 +0900

    idd

--- views/index_view.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from flask import Blueprint, request, render_template, redirect, url_for, session, g
-
-index_blueprint = Blueprint('index', __name__)
-
-@index_blueprint.route('/', methods=['GET'])
-def home():
-    return "do nothing"
